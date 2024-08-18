2feb511ceae9056c0542352e9d741fa3accbc022
unknown <you@example.comhusniddinxomidov2002@gmail.com>
2024-08-18 19:29:23 +0500

    some added items

---
+++ src/components/BaseMainPro/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-[#f5f8f3] border-l-8 border-l-[#FFA726] rounded-3xl w-full flex flex-col sm:flex-row sm:items-center overflow-hidden">
+    <div class="sm:w-1/2 p-4">
+      <p class="text-[#8A9C93]">Date</p>
+      <p class="text-[#252525] sm:text-3xl font-bold">Loyiha nomi</p>
+      <p class="text-[#252525] text-xs sm:text-base">Loyiha haqida qisqacha ma'lumot</p>
+    </div>
+    <div class="sm:w-1/2 w-full">
+      <img src="/advices/1.jpg" alt="" class="object-cover object-center w-full h-full overflow-hidden">
+    </div>
+  </div>
+</template>
--- src/views/home/ourProjects.vue
+++ src/views/home/ourProjects.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import OurProjects from '@/components/BaseMainPro/index.vue'
+</script>
 <template>
   <div class="container mx-auto px-5 pb-14">
     <p class="text-3xl font-semibold px-5 border-l-2 py-5 border-l-blue-600">Bizning loyihalarimiz</p>
-    <div>
-      
+    <div class="mt-11">
+      <OurProjects />
     </div>
   </div>
 </template>
